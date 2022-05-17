4529edd5d175ec55082cf3a94359ad191b960f56
HUISU9903 <aperture0421@gmail.com>
2022-05-18 00:25:23 +0900

    test

---
+++ app/main.py
@@ -0,0 +1,96 @@
+from flask import Flask, request
+import json
+
+# 메인 로직!! 
+def cals(opt_operator, number01, number02):
+    if opt_operator == "addition":
+        return number01 + number02
+    elif opt_operator == "subtraction": 
+        return number01 - number02
+    elif opt_operator == "multiplication":
+        return number01 * number02
+    elif opt_operator == "division":
+        return number01 / number02
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+# 카카오톡 텍스트형 응답
+@app.route('/api/sayHello', methods=['POST'])
+def sayHello():
+    body = request.get_json() # 사용자가 입력한 데이터
+    print(body)
+    print(body['userRequest']['utterance'])
+
+    responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": "안녕 hello I'm Ryan"
+                    }
+                }
+            ]
+        }
+    }
+
+    return responseBody
+
+
+# 카카오톡 이미지형 응답
+@app.route('/api/showHello', methods=['POST'])
+def showHello():
+    body = request.get_json()
+    print(body)
+    print(body['userRequest']['utterance'])
+
+    responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleImage": {
+                        "imageUrl": "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
+                        "altText": "hello I'm Ryan"
+                    }
+                }
+            ]
+        }
+    }
+
+    return responseBody
+
+
+# 카카오톡 Calculator 계산기 응답
+@app.route('/api/calCulator', methods=['POST'])
+def calCulator():
+    body = request.get_json()
+    print(body)
+    params_df = body['action']['params']
+    print(type(params_df))
+    opt_operator = params_df['operators']
+    number01 = json.loads(params_df['sys_number01'])['amount']
+    number02 = json.loads(params_df['sys_number02'])['amount']
+
+    print(opt_operator, type(opt_operator), number01, type(number01))
+
+    answer_text = str(cals(opt_operator, number01, number02))
+
+    responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": answer_text
+                    }
+                }
+            ]
+        }
+    }
+
+    return responseBody
