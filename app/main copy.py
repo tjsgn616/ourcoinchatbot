30854136dd45aff5676d8d65f1fae7137ae0502f
HUISU9903 <aperture0421@gmail.com>
2022-05-18 04:04:32 +0900

    updated

--- app/main copy.py
+++ app/main copy.py
@@ -40,10 +40,21 @@ def msg():
     ## 여기까지 데이터 받아오는거..
 
     title_coin = ["비트코인","이더리움","도지코인","새벽 2시 28분"]
-    
+
     ChoiceUrl=""
     ChoiceDay=0
     ChoiceRes=0
+
+    jsonChoiceDay = {
+    "version": "2.0",
+    "template": {"outputs": [{"simpleText": {"text": "날짜를 선택해 주세요"}}],
+                 "quickReplies": [{"label": "이더리움", "action": "message", "messageText": "이더리움"}
+                 
+                                  ]
+                 }
+}
+
+
 
     coin_name = dataReceive["userRequest"]["utterance"].lower().replace(" ","")
     #coin_name = dataReceive["action"]["detailparams"]["koreanname"]['value'] # 에반데
