62c71cb58820ec7608746dea6fb626d8d3586627
HUISU <aperture0421@gmail.com>
2022-05-20 12:17:19 +0900

    updated

--- jobs.py
+++ jobs.py
@@ -1,11 +1,26 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-import time
+from flask import Flask, make_response, jsonify, request
 sched = BlockingScheduler()
-
+app = Flask(__name__)
+@app.route('/msg', methods=['POST'])
 @sched.scheduled_job('interval', seconds=5)
 def print_data():
-	print(f'job1 : {time.strftime("%H:%M:%S")}')
+	dataReceive = request.get_json()
 
+	#print(f'job1 : {time.strftime("%H:%M:%S")}')
+	now_price = {
+            "version": "2.0",
+            "template": {
+            "outputs": [
+            {
+            "simpleText": {
+               "text": "테스트 텍스트"
+                    }
+                }
+            ]
+                }
+            }
+	return  jsonify(now_price)
 
 
 sched.start()
