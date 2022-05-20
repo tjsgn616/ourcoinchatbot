82c3aa4c02d38caa5228cb7ffa630c072141fee3
HUISU <aperture0421@gmail.com>
2022-05-20 12:22:11 +0900

    updated

--- jobs.py
+++ jobs.py
@@ -3,10 +3,11 @@ from flask import Flask, make_response, jsonify, request
 sched = BlockingScheduler()
 app = Flask(__name__)
 @app.route('/msg', methods=['POST'])
-@sched.scheduled_job('interval', seconds=5)
+@sched.scheduled_job('interval', seconds=3)
 def print_data():
 	#print(f'job1 : {time.strftime("%H:%M:%S")}')
-	now_price = {
+	with sched.app_context()
+		now_price = {
             "version": "2.0",
             "template": {
             "outputs": [
@@ -18,7 +19,7 @@ def print_data():
             ]
                 }
             }
-	return  jsonify(now_price)
+		return  jsonify(now_price)
 
 
 sched.start()
