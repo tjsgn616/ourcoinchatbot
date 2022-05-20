fda4ca30943a5f06614780fb3c759edf15267dc8
HUISU <aperture0421@gmail.com>
2022-05-20 16:00:02 +0900

    updated

--- app/KRW.py
+++ app/KRW.py
@@ -6,41 +6,16 @@ app = Flask(__name__)
 def message():
 
     price_up =  {
-  "version": "2.0",
-  "template": {
-    "outputs": [
-      {
-        "basicCard": {
-          "title": "보물상자",
-          "description": "보물상자 안에는 뭐가 있을까",
-          "thumbnail": {
-            "imageUrl": "http://k.kakaocdn.net/dn/83BvP/bl20duRC1Q1/lj3JUcmrzC53YIjNDkqbWK/i_6piz1p.jpg"
-          },
-          "profile": {
-            "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4BJ9LU4Ikr_EvZLmijfcjzQKMRCJ2bO3A8SVKNuQ78zu2KOqM",
-            "nickname": "보물상자"
-          },
-          "social": {
-            "like": 1238,
-            "comment": 8,
-            "share": 780
-          },
-          "buttons": [
+    "version": "2.0",
+    "template": {
+        "outputs": [
             {
-              "action": "message",
-              "label": "열어보기",
-              "messageText": "짜잔! 우리가 찾던 보물입니다"
-            },
-            {
-              "action":  "webLink",
-              "label": "구경하기",
-              "webLinkUrl": "https://e.kakao.com/t/hello-ryan"
+                "simpleImage": {
+                    "imageUrl": "http://file.mk.co.kr/meet/neds/2022/02/image_readtop_2022_178086_16456851034956965.jpg"
+                }
             }
-          ]
-        }
-      }
-    ]
-  }
+        ]
+    }
 }
     return  jsonify(price_up)
 
