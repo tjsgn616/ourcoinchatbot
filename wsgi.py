31acff10ed3d7378a55f2a107668eb0c591fba77
HUISU9903 <aperture0421@gmail.com>
2022-05-23 03:27:36 +0900

    message

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,3 @@
-from app.sc import app
-from app.weather import app2
+from app.weather_currency import app
 if __name__ == "__main__":
     app.run(threaded=True, port=5000)
-    app2.run(threaded=True, port=5000)
