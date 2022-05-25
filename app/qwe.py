e6aaeaa3dee8c08f251766c4deb2539d3d3911fe
HUISU9903 <aperture0421@gmail.com>
2022-05-25 19:32:48 +0900

    updated

--- app/qwe.py
+++ app/qwe.py
@@ -2,7 +2,7 @@ import pyupbit
 import requests
 import pandas as pd
 import json
-import datetime
+import datetime 
 import project_def as pr_d
 import time
 import test as ts
@@ -33,11 +33,21 @@ def func_9():
         past_price_20 = (pyupbit.get_ohlcv(f'{i}', interval="minute60", to=f'{pastDatetime_YYYY}{pastDatetime_md}{pastDatetime_HM}', count=1).open[0])
         time.sleep(0.05)
         print(i)
+        sun = [] 
+        sun2 = [] 
+        sun3 = [] 
+        sun4 = []
+        cloud = [] 
+        cloud2 = [] 
+        cloud3 = [] 
+        cloud4= []
         past_time2 = 0
-        for num1 in 12,10,8,6,4,2,0 : #[(12,10),(10,8),(8,6),(6,4),(4,2),(2,0)]:#([2,4,6,8,10,12], [0,2,4,6,8,10]:)]
+        for num1 in range(0,26): #[(12,10),(10,8),(8,6),(6,4),(4,2),(2,0)]:#([2,4,6,8,10,12], [0,2,4,6,8,10]:)]
             past_time1 = past_time2
-            past_time2 = (datetime.datetime.now()+datetime.timedelta(hours=-num1)).strftime('%Y%m%d%H%M')
-            print(past_time2)
+            #test = datetime(2022, 5, 25, 9, 00, 00)
+            past_time2 = (datetime.datetime(2022, 5, 25, 8, 00, 00) +datetime.timedelta(hours=+num1) - datetime.timedelta(days = 1)).strftime('%Y%m%d%H%M')
+            #print(past_time2)
+            
             if past_time1 == 0:
                 continue
             elif past_time1 != 0:
@@ -47,14 +57,27 @@ def func_9():
                 time.sleep(0.05)
                 past_price2 = (pyupbit.get_ohlcv(f'{i}', interval="minute60", to=f'{past_time2}', count=1).open[0])
                 time.sleep(0.05)
+                
                 if past_price2 >= past_price1:
-                    print(past_d1,'일',past_H1,'시: ''\033[31m',past_price1,round(100*(past_price2-past_price1)/past_price1,3),'%, 맑음''\033[0m')
+                    sun.append(past_d1)
+                    sun2.append(past_H1)
+                    sun3.append(past_price1)
+                    sun4.append(round(100*(past_price2-past_price1)/past_price1,3))
+                    #print(past_d1,'일',past_H1,'시: ''\033[31m',past_price1,round(100*(past_price2-past_price1)/past_price1,3),'%, 맑음''\033[0m')
                 else:
-                    print(past_d1,'일',past_H1,'시: ''\033[34m',past_price1,round(100*(past_price2-past_price1)/past_price1,3),'%, 흐림''\033[0m')
-        if current_price >= past_price_20:
-            print(past_d,'일',past_h,'시 ->',now_d,'일',now_h,'시 :',past_price_20,'->''\033[31m',current_price, round(100*(current_price-past_price_20)/past_price_20,3),'% 20~ 09 날씨는 맑음''\033[0m')
-        else:
-            print(past_d,'일',past_h,'시 ->',now_d,'일',now_h,'시 :',past_price_20,'->''\033[34m',current_price, round(100*(current_price-past_price_20)/past_price_20,3),'% 20~ 09 날씨는 흐림''\033[0m')
+                    cloud.append(past_d1)
+                    cloud2.append(past_H1)
+                    cloud3.append(past_price1)
+                    cloud4.append(round(100*(past_price2-past_price1)/past_price1,3))
+
+                    #print(past_d1,'일',past_H1,'시: ''\033[34m',past_price1,round(100*(past_price2-past_price1)/past_price1,3),'%, 흐림''\033[0m')
+
+            print(sun2)
+            print(cloud2)
+        #if current_price >= past_price_20:
+            #print(past_d,'일',past_h,'시 ->',now_d,'일',now_h,'시 :',past_price_20,'->''\033[31m',current_price, round(100*(current_price-past_price_20)/past_price_20,3),'% 20~ 09 날씨는 맑음''\033[0m')
+       # else:
+            #print(past_d,'일',past_h,'시 ->',now_d,'일',now_h,'시 :',past_price_20,'->''\033[34m',current_price, round(100*(current_price-past_price_20)/past_price_20,3),'% 20~ 09 날씨는 흐림''\033[0m')
     print("테스트")
 func_9()
 #func_14()
--- app/sc.py
+++ app/sc.py
@@ -1,105 +1,35 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 @app.route('/price', methods=['POST'])
-def func_9():
-    
-    qpqwe = {
-  "version": "2.0",
-  "template": {
-    "outputs": [
-      {
-        "carousel": {
-          "type": "listCard",
-          "items": [
-            {
-              "header": {
-                "title": "샌드위치"
-              },
-              "items": [
-                {
-                  "title": "햄치즈",
-                  "description": "4,500원",
-                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_01.jpg"
-                },
-                {
-                  "title": "베이컨 아보카도",
-                  "description": "5,500원",
-                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_02.jpg"
-                },
-                {
-                  "title": "에그 포테이토",
-                  "description": "5,300원",
-                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_03.jpg"
-                },
-                {
-                  "title": "갈릭 베이컨 토마토",
-                  "description": "5,800원",
-                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_04.jpg"
-                }
-              ],
-              "buttons": [
-                {
-                  "label": "더보기",
-                  "action": "message",
-                  "messageText" : "샌드위치 더보기"
-                }
-              ]
-            },
-            {
-              "header": {
-                "title": "커피"
-              },
-              "items": [
-                {
-                  "title": "아메리카노",
-                  "description": "1,800원",
-                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_05.jpg"
-                },
-                {
-                  "title": "카페라떼",
-                  "description": "2,000원",
-                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_06.jpg"
-                },
-                {
-                  "title": "카페모카",
-                  "description": "2,500원",
-                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_07.jpg"
-                },
-                {
-                  "title": "소이라떼",
-                  "description": "2,200원",
-                  "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/02_img_08.jpg"
-                }
-              ],
-              "buttons": [
-                {
-                  "label": "더보기",
-                  "action": "message",
-                  "messageText" : "커피 더보기"
-                }
-              ]
+def Message():
+    content = request.get_json()
+    content = content['userRequest']['utterance']
+    content=content.replace("\n","")
+    print(content)
+    if content == u"오늘의 메뉴":
+        dataSend = {
+            "version" : "2.0",
+            "template" : {
+                "outputs" : [
+                    {
+                        "simpleText" : {
+                            "text" : "테스트입니다."
+                        }
+                    }
+                ]
             }
-          ]
         }
-      }
-    ],
-    "quickReplies": [
-      {
-        "messageText": "인기 메뉴",
-        "action": "message",
-        "label": "인기 메뉴"
-      },
-      {
-        "messageText": "최근 주문",
-        "action": "message",
-        "label": "최근 주문"
-      },
-      {
-        "messageText": "장바구니",
-        "action": "message",
-        "label": "장바구니"
-      }
-    ]
-  }
-}
-    return (qpqwe)
+    else:
+        dataSend = {
+            "version" : "2.0",
+            "template" : {
+                "outputs" : [
+                    {
+                        "simpleText" : {
+                            "text" : "error입니다."
+                        }
+                    }
+                ]
+            }
+        }
+    return jsonify(dataSend)
