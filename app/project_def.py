603f861f7a6a68b85e34b070e55c118f2ef45082
HUISU <aperture0421@gmail.com>
2022-05-27 14:18:43 +0900

    updated

--- app/project_def.py
+++ app/project_def.py
@@ -1,9 +1,22 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-schedule = BackgroundScheduler()
+import time
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-def CheckPrice():
-    print("Running")
+sched = BlockingScheduler()
 
-schedule.add_job(func = CheckPrice,trigger='interval',minutes = 1)
-schedule.start()
+# 매일 12시 30분에 실행
+@sched.scheduled_job('interval', seconds=5, id='test_1')
+def job1():
+    print(f'job1 : {time.strftime("%H:%M:%S")}')
 
+# 매일 12시 30분에 실행
+@sched.scheduled_job('cron', hour='12', minute='30', id='test_2')
+def job2():
+    print(f'job2 : {time.strftime("%H:%M:%S")}')
+
+# 이런식으로 추가도 가능. 매분에 실행
+sched.add_job(job2, 'cron', second='0', id="test_3")
+
+
+print('sched before~')
+sched.start()
+print('sched after~')
