c08197544cc5415caff0210043ed42614d93e17d
HUISU9903 <aperture0421@gmail.com>
2022-05-29 01:24:55 +0900

    updated

--- app/jobs.py
+++ app/jobs.py
@@ -3,8 +3,8 @@
 #@app.route('/price', methods=['POST'])
 import pandas as pd
 def func_11():
-    #csv_data = pd.read_csv("./app/filename.csv") 
-    csv_data = pd.read_csv("filename.csv")
+    csv_data = pd.read_csv("./app/filename.csv") 
+    #csv_data = pd.read_csv("filename.csv")
     print(csv_data)
 func_11()
     
