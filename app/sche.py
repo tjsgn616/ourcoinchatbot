945f652779208a6bbf18aa905300ec4a494e9e27
HUISU9903 <aperture0421@gmail.com>
2022-05-29 14:48:09 +0900

    updated

--- app/sche.py
+++ app/sche.py
@@ -1,23 +1,25 @@
 import time
 from apscheduler.schedulers.blocking import BlockingScheduler
-import KRW
+#import KRW
 import jobs
 sched = BlockingScheduler(timezone='Asia/Seoul')
 
 # 매일 12시 30분에 실행
-@sched.scheduled_job('interval', seconds=5, id='test_1')
-def job1():
-    KRW.func_9()
+#@sched.scheduled_job('interval', seconds=5, id='test_1')
+#def job1():
+    #KRW.func_9()
     #print(f'job1 : {time.strftime("%H:%M:%S")}')
 
 
 # 매일 12시 30분에 실행
-@sched.scheduled_job('interval', seconds=10, id='test_2')
-#@sched.scheduled_job('cron', second='10', id='test_2')
+#@sched.scheduled_job('interval', seconds=10, id='test_2')
+@sched.scheduled_job('cron', second='10', id='test_2')
 def job2():
     jobs.func_11()
     #print(f'job2 : {time.strftime("%H:%M:%S")}')
-
+@sched.scheduled_job('cron', second='10', id='test_3')
+def job3():
+    print(time.strftime("%H:%M:%S"))
 # 이런식으로 추가도 가능. 매분에 실행
 
 #sched.add_job(job2, 'cron', second='0', id="test_3")
