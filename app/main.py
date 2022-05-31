97ba06ea79e3e4ff3334a364c7e2b568f50c1d53
HUISU <aperture0421@gmail.com>
2022-05-31 11:17:41 +0900

    updated

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ from bs4 import BeautifulSoup
 import re
 import urllib.request
 from urllib.parse import quote
-
+from app import top5
 
 app = Flask(__name__)
 
@@ -296,8 +296,9 @@ def test():
 def acc():
     # market 한국 이름 뽑아내기
     nameData = marketData()
-    top_change = pd.read_csv("app/data/top_change.csv")
-    top_live = pd.read_csv("app/data/live_top.csv")
+    top_live, top_change= top5.si()
+    #top_change = pd.read_csv("app/data/top_change.csv")
+    #top_live = pd.read_csv("app/data/live_top.csv")
     top_market_list = pd.read_csv("app/data/market_list.csv")
     # -------------------------------------------------if문 top_market_list nameData로 바꾸기
     top_change_kor = []
